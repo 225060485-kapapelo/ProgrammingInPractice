1e2672164df4cef3f044960394e5485cc818eb19
225060485-kapapelo <225060485@nust.na>
2026-08-10 16:19:09 +0200

    Week 1 - First C Program

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 int main(){
-  printf("Welcome to windhoek munipality\n");
-  return 0;
+    char municipal_name[20], mayor_name[20];
+    int population;
+
+    printf("Enter the municipality name:\n");
+    scanf("%19s", &municipal_name);
+
+    printf("Enter the Mayor's name:\n");
+    scanf("%19s", &mayor_name);
+
+    printf("Enter the population of the municipality:\n");
+    scanf("%d", &population);
+
+    printf("municipality name: %s\n", municipal_name);
+    printf("mayor's name: %s\n", mayor_name);
+    printf("population: %d\n", population);
+
+    return 0;
+    
 }
