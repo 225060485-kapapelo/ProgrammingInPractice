5d8f322d1b2cb57bff51bcce269a8a56ab272d4d
225060485-kapapelo <225060485@nust.na>
2026-08-17 09:48:46 +0200

    Week 1 - Municipal Financial Management System

--- main.c
+++ main.c
@@ -2,19 +2,27 @@
 int main(){
     char municipal_name[20], mayor_name[20];
     int population;
-
+    // System title
+    printf("===============================\n");
+    printf("MUNICIPAL FINANCIAL MANAGEMENT\n");
+    printf("===============================\n");
+     //Welcome message 
+    printf("Welcome to windhoek municipality\n");
     printf("Enter the municipality name:\n");
     scanf("%19s", municipal_name);
 
     printf("Enter the Mayor's name:\n");
-    scanf("%19s", mayor_name);
+    scanf(" %19[^\n]", mayor_name);
 
     printf("Enter the population of the municipality:\n");
-    scanf("%d", &population);
-
+    scanf(" %d", &population);
+    printf("================================\n");
+    printf("  WINDHOEK MUNICIPALITY INFORMATION\n");
+    printf("================================\n");
     printf("municipality name: %s\n", municipal_name);
     printf("mayor's name: %s\n", mayor_name);
     printf("population: %d\n", population);
 
+    printf("===============================\n");
     return 0;
 }
